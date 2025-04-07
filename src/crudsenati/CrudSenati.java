4ca33498680b153a759cc56c4c39057ecf5cc628
Alexander <1602402@senati.pe>
2025-04-06 19:10:16 -0500

    "Actualizacion del conexion al crud senati"

--- src/crudsenati/CrudSenati.java
+++ src/crudsenati/CrudSenati.java
@@ -15,6 +15,9 @@ public class CrudSenati {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Conexion cn = new Conexion();
+
+        cn.conectar();
     }
     
 }
